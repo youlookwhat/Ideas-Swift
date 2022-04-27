661cc4f4b971285190c1ff33ffe1be53789e3d40
jngbin <770413277@qq.com>
2022-04-27 21:02:00 +0800

    add 调试请求

--- quote/quote/ViewController.swift
+++ quote/quote/ViewController.swift
@@ -16,16 +16,38 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // 解析：https://juejin.cn/post/6875140053635432462
         let url = "http://v3.wufazhuce.com:8000/api/channel/one/0/0"
-        AF.request(url).response { result in
-           if let obj = result.value {
+        let parameters = ["foo": "bar"]
+        
+//        AF.request(url,parameters: parameters, encoder: JSONParameterEncoder.default).response { result in
+            
+        AF.request(url).responseJSON { result in
+//           if let obj = result.value {
 //           let dict = obj as! NSDictionary
-           print(obj)
+            
+            print(result)
 //           self.newsContent = NewsContent(dict: dict)
-            }
+//            }
         }
+        
+//        let login = Login(email: "test@test.test", password: "testPassword")
+//
+//        AF.request("https://httpbin.org/post",
+//                   method: .post,
+//                   parameters: login,
+//                   encoder: JSONParameterEncoder.default).response { response in
+//            debugPrint(response)
+//        }
+
+        
     }
 
 
+}
+
+struct Login: Encodable {
+    let email: String
+    let password: String
 }
 
