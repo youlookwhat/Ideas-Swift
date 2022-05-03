162b45557549ed510c75d37e42f46289dee5a40a
youlookwhat <770413277@qq.com>
2022-05-03 21:18:03 +0800

    add app icon

--- quote/quote/AppDelegate.swift
+++ quote/quote/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+//import Kingfisher
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // 显示2秒
+//        Thread.sleep(forTimeInterval: 2)
+//        showLauchImage()
         return true
     }
 
@@ -32,6 +37,102 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func showLauchImage() {
+            let url_string = "http://image.wufazhuce.com/FqWc5Ddw32045kUodXqxIdOujndM"
+            
+    //        Alamofire.request(url_string,
+    //               method: .get,
+    //               encoder: JSONParameterEncoder.default).response { response in
+    //        debugPrint(response)
+            
+            
+//            AF.request(url_string).response { result in
+//                debugPrint(result)
+    //        }
+    //
+    //        AF.request(url_string, method: .get, parameters: nil, encoder: JSONParameterEncoder.default).response{ (response) in
+                
+    //        }
+    //        AF.request(.GET, url_string).responseJSON { (_, _, result) -> Void in
+//                if let obj = result.value {
+//                    let dict = obj as! NSDictionary
+                    
+                    let screen_size = UIScreen.main.bounds.size
+                    
+//                    let img = UIImageView(frame:CGRectMake(0, 0, screen_size.width, screen_size.height))
+                    let img = UIImageView(frame:CGRect(x: 0, y: 0, width: screen_size.width, height: screen_size.height))
+                    img.image = UIImage(named: "app_lauch")
+                    img.backgroundColor = UIColor.black
+                    img.contentMode = .scaleAspectFit
+        
+                    
+        let window = UIApplication.shared.keyWindow
+        window!.addSubview(img)
+//        window.append(img)
+                    
+//                    let image_url_string = dict["img"] as! String
+                    
+        
+//                    let url = URL(string: url_string)
+//                    img.kf.setImage(with: url)
+        
+        
+
+        
+//                    AF.request(url_string).response {result in
+//                        if let image_data = result {
+//                            img.image = UIImage(data: result)
+//                        }
+//                    }
+        
+//        AF.request(url_string).response(completionHandler: { (_,_,data,_) -> Void in
+//            if let image_data = data {
+//                img.image = UIImage(data: image_data)
+//            }
+//        })
+    //                Alamofire.request(.GET, image_url_string).response(completionHandler: { (_, _, data, _) -> Void in
+    //                    if let image_data = data {
+    //                        img.image = UIImage(data: image_data)
+    //                    }
+    //                })
+                    
+                    let lbl = UILabel(frame:CGRect(x: 0, y:screen_size.height-50, width: screen_size.width, height: 20))
+    //                let lbl = UILabel(frame:CGRectMake(0, screen_size.height-50, screen_size.width, 20))
+                    lbl.backgroundColor = UIColor.clear
+//                    lbl.text = dict["text"] as? String
+        lbl.text = "跳过"
+                    lbl.textColor = UIColor.gray
+                    lbl.textAlignment = NSTextAlignment.center
+                    lbl.font = UIFont.systemFont(ofSize: 14)
+                    window!.addSubview(lbl)
+                    
+                    UIView.animate(withDuration: 3,animations:{
+                        let height = UIScreen.main.bounds.size.height
+                        let rect = CGRect(x: -100, y:-100, width: screen_size.width+200, height: height+200)
+    //                    let rect = CGRectMake(-100,-100, screen_size.width+200, height+200)
+                        img.frame = rect
+                        },completion:{
+                            (completion) in
+                            
+                            if completion {
+                                UIView.animate(withDuration: 1,animations:{
+                                    img.alpha = 0
+                                    lbl.alpha = 0
+                                    },completion:{
+                                        (completion) in
+                                        
+                                        if completion {
+                                            img.removeFromSuperview()
+                                            lbl.removeFromSuperview()
+                                        }
+                                })
+                            }
+                    })
+                    
+                }
+                
+//            }
+//        }
 
 }
 
