9b4bff690420450332eba01adf9acf57b0d04c88
youlookwhat <770413277@qq.com>
2022-06-24 22:57:35 +0800

    add 增加返回事件

--- quote/quote/ViewController/WebViewViewController.swift
+++ quote/quote/ViewController/WebViewViewController.swift
@@ -11,16 +11,49 @@ import WebKit
 
 class WebViewViewController: UIViewController, WKScriptMessageHandler {
 
+    var imageView:UIImageView! = nil
+    var titleView:UILabel! = nil
     var webview:WKWebView! = nil
     var bt1:UIButton? = nil
     var bt2:UIButton? = nil
     var progress:UIProgressView! = nil
     var url:String? = nil
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.tabBarItem.title = "网页"
+        
+//        title = "网页"
+        //添加取消btn
+//        let image = UIImage(named: "icon_title_back")
+//        let image2 = resizeImage(image: image!, targetSize: CGSize(width: 44, height: 44))
+        
+//        let backBt = UIBarButtonItem(image: image2, style: .plain, target: self, action: #selector(navBtnPress))
+//        backBt.tag = 1000
+//        self.navigationItem.leftBarButtonItem = backBt
+        
+//        let backBt = UIBarButtonItem(title: "取消", style: .plain, target: self, action: #selector(navBtnPress))
+//        backBt.tag = 1000
+//        self.navigationItem.leftBarButtonItem = backBt
+//
+//        let backBt = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(navBtnPress))
+//        backBt.tag = 1000
+//        self.navigationItem.leftBarButtonItem = backBt
+        
+//        navigationItem.leftBarButtonItem?.action = #selector(navBtnPress)
+        
+        // 隐藏标题栏
+//        navigationController?.navigationBar.isHidden = true
+        
+//        let singleTapGesture = UITapGestureRecognizer(target: self, action: #selector(navBtnPress))
+//        navigationItem.leftBarButtonItem!.addTarget(self, action: #selector(navBtnPress))
+//        navigationItem.leftBarButtonItem!.addGestureRecognizer(singleTapGesture)
+//        navigationController?.navigationBar.backIndicatorImage!.addGestureRecognizer(singleTapGesture)
+//        navigationController?.navigationBar.backIndicatorImage!.isUserInteractionEnabled = true
+        
+//        self.tabBarItem.title = ""
         view.backgroundColor = UIColor(lightThemeColor: .white, darkThemeColor: .black)
         
 
@@ -89,23 +122,44 @@ class WebViewViewController: UIViewController, WKScriptMessageHandler {
     
     func addToolView(){
         // 创建工具条
-        let toolView = UIView(frame: CGRect(x: 0, y: self.view.frame.size.height-120, width: self.view.frame.width, height: 30))
-        toolView.backgroundColor = UIColor.purple
+        let toolView = UIView(frame: CGRect(x: 0, y: statusBarHeight, width: self.view.frame.width, height: 44))
+//        toolView.backgroundColor = UIColor.white
         self.view.addSubview(toolView)
         // 添加返回按钮
-        bt1 = UIButton(frame: CGRect(x: 30, y: 0, width: 70, height: 30))
+        bt1 = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
         bt1!.setTitle("后退", for: .normal)
         bt1!.setTitleColor(UIColor.lightGray, for: .disabled)
         bt1!.addTarget(self, action: #selector(back), for: .touchUpInside)
+        bt1?.isHidden = true
+        
+        
+        // 返回图片
+        let image = UIImage(named: "iv_left_back")
+        imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        // 给图片加点击事件
+        let singleTapGesture = UITapGestureRecognizer(target: self, action: #selector(navBtnPress))
+        imageView.addGestureRecognizer(singleTapGesture)
+        imageView.isUserInteractionEnabled = true
+        toolView.addSubview(imageView)
+        
+        titleView = UILabel(frame: CGRect(x: 44, y: 0, width: kScreenWidth - 88, height: 44))
+        titleView.textColor = UIColor(lightThemeColor: .black, darkThemeColor: .white)
+        titleView.font = .systemFont(ofSize: 16, weight: .medium)
+        toolView.addSubview(titleView)
+        
         // 添加前进按钮
         bt2 = UIButton(frame: CGRect(x: 130, y: 0, width: 70, height: 30))
         bt2!.setTitle("前进", for: .normal)
         bt2!.setTitleColor(UIColor.lightGray, for: .disabled)
         bt2!.addTarget(self, action: #selector(goForward), for: .touchUpInside)
+        bt2!.isHidden = true
+        
         toolView.addSubview(bt1!)
         toolView.addSubview(bt2!)
         
-        toolView.isHidden = true
+//        toolView.isHidden = true
         
         // 加载进度条
         progress = UIProgressView(frame: CGRect(x: 0, y: kNavigationBarHeight, width: self.view.frame.size.width, height: 1))
@@ -115,32 +169,60 @@ class WebViewViewController: UIViewController, WKScriptMessageHandler {
         progress.trackTintColor = UIColor.clear
         progress!.progress = 0
         self.view.addSubview(progress!)
+        // 监听进度条
         webview?.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+        // 监听标题
+        webview?.addObserver(self, forKeyPath: "title", options: .new, context: nil)
         
      }
     
     
     // 监听网页进度的变化
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        progress?.progress = Float(webview!.estimatedProgress)
-        print("----进度 \(progress!.progress)")
-        if progress!.progress==1 {
-            // 延迟2秒显示webview
-            webview.isHidden = false
-            
-            progress.isHidden = true
-            if #available(iOS 13.0, *) {
-                if (traitCollection.userInterfaceStyle == .dark){
-                    changeTextBackgroundStyle()
-                } else {
-                    changeTextBackgroundStyle(style: "light")
+//        title = webview!.title
+        
+        if "title" == keyPath {
+            titleView.text = webview!.title
+        } else if "estimatedProgress" == keyPath {
+            progress?.progress = Float(webview!.estimatedProgress)
+            print("----进度 \(progress!.progress)")
+            if progress!.progress==1 {
+                // 延迟2秒显示webview
+                webview.isHidden = false
+                
+                progress.isHidden = true
+                if #available(iOS 13.0, *) {
+                    if (traitCollection.userInterfaceStyle == .dark){
+                        changeTextBackgroundStyle()
+                    } else {
+                        changeTextBackgroundStyle(style: "light")
+                    }
                 }
+            } else {
+                progress.isHidden = false
             }
-        } else {
-            progress.isHidden = false
         }
     }
         
+    @objc func navBtnPress(){
+        if webview!.canGoBack {
+            webview?.goBack()
+        } else {
+            // 退出页面
+            if presentingViewController != nil && presentingViewController?.presentedViewController == self {
+                    dismiss(animated: true, completion: nil)
+                } else if navigationController?.presentingViewController != nil && navigationController?.presentingViewController?.presentedViewController == navigationController {
+                    if navigationController?.children.count ?? 0 > 1 {
+                        navigationController?.popViewController(animated: true)
+                    } else {
+                        navigationController?.dismiss(animated: true, completion: nil)
+                    }
+                } else {
+                    navigationController?.popViewController(animated: true)
+                }
+        }
+    }
+    
     // 后退
     @objc func back(){
         if webview!.canGoBack {webview?.goBack()}
@@ -176,9 +258,12 @@ class WebViewViewController: UIViewController, WKScriptMessageHandler {
            
            self.webview.evaluateJavaScript("document.body.style.webkitTextFillColor=\"#7E9EB3\"", completionHandler: nil)
     
+//           imageView?.tintColor = .white
        }else{
            self.webview.evaluateJavaScript("document.getElementsByTagName('body')[0].style.webkitTextFillColor= '#000000'", completionHandler: nil)
            self.webview.evaluateJavaScript("document.body.style.backgroundColor=\"#FFFFFF\"", completionHandler: nil)
+           
+//           imageView?.image = .black
 
        }
    }
@@ -192,4 +277,29 @@ class WebViewViewController: UIViewController, WKScriptMessageHandler {
         webview = nil
     }
     
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage? {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(origin: .zero, size: newSize)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
 }
